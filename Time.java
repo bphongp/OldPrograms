44550c59432df15d235ad268b988a95e0a37a4fd
Bamphiane Annie Phongphouthai <31457337+bphongp@users.noreply.github.com>
2017-09-08 14:59:37 -0400

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,98 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class Time
+{
+	Scanner read = new Scanner(System.in);
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public Time()
+	{
+		this(0,0,0);
+	}
+	public Time(int hour, int minute, int second)
+	{
+		if(hour<0 || hour>= 24)
+			throw new IllegalArgumentException("hour must be 0-23");
+		if(minute<0 || minute >= 60)
+			throw new IllegalArgumentException("minute must be 0-60");
+		if(second< 0 || second>= 60)
+			throw new IllegalArgumentException("second must be 0-60");
+		
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	public Time(Time time)
+	{
+	    this(time.getHour(), time.getMinute(), time.getSecond());
+	} 
+	public void setTime(int hour, int minute, int second)
+	{
+		if(hour<0 || hour>= 24)
+			throw new IllegalArgumentException("hour must be 0-23");
+		if(minute<0 || minute >= 60)
+			throw new IllegalArgumentException("minute must be 0-60");
+		if(second< 0 || second>= 60)
+			throw new IllegalArgumentException("second must be 0-60");
+		
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	public void setHour(int hour)
+	{
+		if (hour < 0 || hour >= 24)
+	         throw new IllegalArgumentException("hour must be 0-23");
+
+	      this.hour = hour;
+	}
+	public void setMinute(int minute)
+	{
+		if (minute < 0 || minute >= 60)
+	         throw new IllegalArgumentException("minute must be 0-59");
+
+	      this.minute = minute; 
+	}
+	public void setSecond(int second)
+	{
+		if (second < 0 || second >= 60)
+	         throw new IllegalArgumentException("second must be 0-59");
+
+	       this.second = second; 
+	}
+	public int getHour()
+	{
+		return hour;
+	}
+	public int getMinute()
+	{
+		return minute;
+	}
+	public int getSecond()
+	{
+		return second;
+	}
+	public void readTime()
+	{
+		System.out.println("	Hour: ");
+		setHour(read.nextInt());
+		System.out.println("	Minute: ");
+		setMinute(read.nextInt());
+		System.out.println("	Second: ");
+		setSecond(read.nextInt());
+	}
+	public String toUniversalString()
+	{
+		return String.format("%02d:%02d:%02d", getHour(), getMinute(), getSecond());
+	}
+	public String toString()
+	{
+		return String.format("%d:%02d:%02d %s", 
+		         ((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12),
+		         getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
+	}
+}
